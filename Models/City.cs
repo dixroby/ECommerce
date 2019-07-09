8f91d411753649e52a1bdea643799738085e1494
dixroby <alestilodefe@gmail.com>
2019-07-09 04:18:11 -0500

    crud de ciudad y departamento

---
+++ Models/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [MaxLength(50, ErrorMessage = "el campo {0} como maximo {1} logitud de caracteres ")]
+
+        //interiomente se ve como name  pero el usuario lo vera como  Ciudad
+        [Display(Name = "Ciudad")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public int Departmentid { get; set; }
+
+        //para hacer la llave
+        public virtual Department Department { get; set; }
+    }
+}
---
+++ Models/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Departmentid { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [MaxLength(50,ErrorMessage ="el campo {0} como maximo {1} logitud de caracteres ")]
+
+        //interiomente se ve como name  pero el usuario lo vera como  departamentos
+        [Display(Name ="Departamento")]
+        public string Name { get; set; }
+        
+        // definiendo uno a muchos en entity framework
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
